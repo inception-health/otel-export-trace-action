8559b24bc3569cdd3aaba692b51a6be70e040588
Nick Harris <nick.harris@froedtert.com>
2022-02-07 12:12:57 -0600

    added tests to build workflow. configured jest-junit

--- src/tracing/trace.test.ts
+++ src/tracing/trace.test.ts
@@ -27,59 +27,62 @@ describe("createTracerProvider", () => {
     jest.useRealTimers();
     return subject.shutdown();
   });
+  describe("resource attributes", () => {
+    it("has service.name resource as workflow name", () => {
+      subject = createTracerProvider(
+        "localhost",
+        "test=foo",
+        mockWorkflowRunJobs
+      );
+      expect(
+        subject.resource.attributes[SemanticResourceAttributes.SERVICE_NAME]
+      ).toEqual(mockWorkflowRunJobs.workflowRun.name);
+    });
 
-  it("has service.name resource as workflow name", () => {
-    subject = createTracerProvider(
-      "localhost",
-      "test=foo",
-      mockWorkflowRunJobs
-    );
-    expect(
-      subject.resource.attributes[SemanticResourceAttributes.SERVICE_NAME]
-    ).toEqual(mockWorkflowRunJobs.workflowRun.name);
-  });
+    it("has service.name resource as workflow id", () => {
+      mockWorkflowRunJobs.workflowRun.name = undefined;
+      subject = createTracerProvider(
+        "localhost",
+        "test=foo",
+        mockWorkflowRunJobs
+      );
+      expect(
+        subject.resource.attributes[SemanticResourceAttributes.SERVICE_NAME]
+      ).toEqual(`${mockWorkflowRunJobs.workflowRun.id}`);
+    });
 
-  it("has service.name resource as workflow id", () => {
-    mockWorkflowRunJobs.workflowRun.name = undefined;
-    subject = createTracerProvider(
-      "localhost",
-      "test=foo",
-      mockWorkflowRunJobs
-    );
-    expect(
-      subject.resource.attributes[SemanticResourceAttributes.SERVICE_NAME]
-    ).toEqual(`${mockWorkflowRunJobs.workflowRun.id}`);
-  });
+    it("has service.instance.id resource", () => {
+      subject = createTracerProvider(
+        "localhost",
+        "test=foo",
+        mockWorkflowRunJobs
+      );
+      expect(
+        subject.resource.attributes[
+          SemanticResourceAttributes.SERVICE_INSTANCE_ID
+        ]
+      ).toEqual(
+        [
+          mockWorkflowRunJobs.workflowRun.repository.full_name,
+          mockWorkflowRunJobs.workflowRun.workflow_id,
+          mockWorkflowRunJobs.workflowRun.id,
+          mockWorkflowRunJobs.workflowRun.run_attempt,
+        ].join("/")
+      );
+    });
 
-  it("has service.instance.id resource", () => {
-    subject = createTracerProvider(
-      "localhost",
-      "test=foo",
-      mockWorkflowRunJobs
-    );
-    expect(
-      subject.resource.attributes[
-        SemanticResourceAttributes.SERVICE_INSTANCE_ID
-      ]
-    ).toEqual(
-      [
-        mockWorkflowRunJobs.workflowRun.repository.full_name,
-        mockWorkflowRunJobs.workflowRun.workflow_id,
-        mockWorkflowRunJobs.workflowRun.id,
-        mockWorkflowRunJobs.workflowRun.run_attempt,
-      ].join("/")
-    );
-  });
-
-  it("has service.namespace resource", () => {
-    subject = createTracerProvider(
-      "localhost",
-      "test=foo",
-      mockWorkflowRunJobs
-    );
-    expect(
-      subject.resource.attributes[SemanticResourceAttributes.SERVICE_NAMESPACE]
-    ).toEqual(mockWorkflowRunJobs.workflowRun.repository.full_name);
+    it("has service.namespace resource", () => {
+      subject = createTracerProvider(
+        "localhost",
+        "test=foo",
+        mockWorkflowRunJobs
+      );
+      expect(
+        subject.resource.attributes[
+          SemanticResourceAttributes.SERVICE_NAMESPACE
+        ]
+      ).toEqual(mockWorkflowRunJobs.workflowRun.repository.full_name);
+    });
   });
 
   it("has active span processor", () => {
