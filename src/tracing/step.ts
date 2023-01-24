8ef4ea0ab7a65ffa2ddfcb8ef9f39b3417b57f15
Steven E. Harris <seh@panix.com>
2023-01-24 18:03:22 -0500

    feat(#31): Omit spans for skipped and canceled jobs (#33)
    * Omit trace spans for canceled or skipped steps
    
    When an GitHub Actions workflow job decides to forgo running a
    particular step, whether due to it being canceled or skipped because
    some precondition fails, do not publish a trace span to represent that
    step having occurred.
    
    * Include step ID as a span attribute when present
    
    For GitHub Actions job steps that include an ID, publish it in the
    respective trace span as the "github.job.step.id" string-typed
    attribute.

--- src/tracing/step.ts
+++ src/tracing/step.ts
@@ -28,7 +28,11 @@ export async function traceWorkflowRunStep({
 }: TraceWorkflowRunStepParams) {
   if (!step || !step.completed_at || !step.started_at) {
     const stepName = step?.name || "UNDEFINED";
-    console.warn(`Step ${stepName} is not completed yet`);
+    console.warn(`Step ${stepName} is not completed yet.`);
+    return;
+  }
+  if (step.conclusion == "cancelled" || step.conclusion == "skipped") {
+    console.info(`Step ${step.name} did not run.`);
     return;
   }
   core.debug(`Trace Step ${step.name}`);
@@ -44,6 +48,7 @@ export async function traceWorkflowRunStep({
         "github.job.step.started_at": step.started_at || undefined,
         "github.job.step.completed_at": step.completed_at || undefined,
         error: step.conclusion === "failure",
+        ...(step.id && { "github.job.step.id": step.id }),
       },
       startTime,
     },
